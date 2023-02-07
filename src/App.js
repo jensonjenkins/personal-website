323edae2f963401b90462791b91cc464f6a2f03c
jensonjenkins <dave.martheen@gmail.com>
2023-02-08 02:10:07 +0800

    react-scripts install

--- src/App.js
+++ src/App.js
@@ -1,48 +1,27 @@
 import Navbar from './components/Navbar'
-import { useState } from 'react';
-import Axios from 'axios';
+
 
 function App() {
-  const [rateUSD, setRateUSD] = useState('')
-  const [rateEUR, setRateEUR] = useState('')
-  const [rateGBP, setRateGBP] = useState('')
-  const [time, setTime] = useState('')
-  const getJoke = () =>{
-    Axios.get('https://api.coindesk.com/v1/bpi/currentprice.json').then((response) =>{
-      console.log(response);
-      
-      setRateUSD(' USD : $' + response.data.bpi.USD.rate)
-      setTime('Time : '+ response.data.time.updated)
-      setRateEUR(' EUR : €' + response.data.bpi.EUR.rate)
-      setRateGBP(' GBP : £' + response.data.bpi.GBP.rate)
-
-    })
-  }
+  
   return (
     <div className="App">
       <Navbar />
       <h1 className='hellotext'>Hello</h1>
 
       <section id='page1'>
-        {/* <h1 className='test1'>About Me</h1> */}
-        <button onClick={getJoke} className="BitButton">Check BitCoin Rate</button>
-        
-        <p>{time}</p>
-        <p>{rateUSD}</p>
-        <p>{rateEUR}</p>
-        <p>{rateGBP}</p>
-        
+        <h1 className='test1'>About Me</h1>
 
       </section>
               
       <section id='page2'>
-        <h1 className='test2'>What I've Achieved</h1>
+        <h1 className='test2'>Experience</h1>
         <p>This is still my website</p>
       </section>
 
       <section id='page3'>
         <h1 className='test3'>Education</h1>
-        <p>This is still my website</p>
+        <p>Nanyang Technological University</p>
+        <p>Information Engineering and Media</p>
       </section>
 
       <section id='page4'>
